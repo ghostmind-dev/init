52968c1fea1245e4fa5e9addd4b3d4ccafe99f26
Francois Seguin <seguin.mtl@gmail.com>
2023-01-02 18:37:36 +0000

    documenting

--- Readme.md
+++ Readme.md
@@ -7,3 +7,14 @@ post-create script for devcontainer
 ```bash
 npm install @ghostmind-dev/post-create
 ```
+
+## environment variable
+
+| name                       | default value | description                                               |
+| -------------------------- | ------------- | --------------------------------------------------------- |
+| `EXPORT_PROJECT_ENV`       | `false`       | export project environment from vault to .env             |
+| `INSTALL_DEV_DEPENDENCIES` | `false`       | install all dev dependencies for all project folders      |
+| `RESET_LIVE_RUN`           | `false`       | remove and reinstall local run repository (liver command) |
+| `SET_NPM_CREDENTIALS`      | `false`       | set npm environment                                       |
+| `SET_GCP_CREDENTIALS`      | `false`       | set gcp environment                                       |
+| `SET_GAM_CREDENTIALS`      | `false`       | set gam environment                                       |
--- src/main.mjs
+++ src/main.mjs
@@ -1,8 +1,8 @@
-import { $, fs, chalk, sleep } from "zx";
-import { config } from "dotenv";
+import { $, fs, chalk, sleep } from 'zx';
+import { config } from 'dotenv';
 
 export default async function postCreate() {
-  console.log(chalk.blue("Starting devcontainer..."));
+  console.log(chalk.blue('Starting devcontainer...'));
 
   //////////////////////////////////////////////////////////////////////////////////
   // CONSTANTS
@@ -17,12 +17,12 @@ export default async function postCreate() {
 
   const {
     GCP_PROJECT_NAME,
-    EXPORT_PROJECT_ENV = "false",
-    INSTALL_DEV_DEPENDENCIES = "false",
-    RESET_LIVE_RUN = "false",
-    SET_NPM_CREDENTIALS = "false",
-    SET_GCP_CREDENTIALS = "false",
-    SET_GAM_CREDENTIALS = "false",
+    EXPORT_PROJECT_ENV = 'false',
+    INSTALL_DEV_DEPENDENCIES = 'false',
+    RESET_LIVE_RUN = 'false',
+    SET_NPM_CREDENTIALS = 'false',
+    SET_GCP_CREDENTIALS = 'false',
+    SET_GAM_CREDENTIALS = 'false',
   } = process.env;
 
   //////////////////////////////////////////////////////////////////////////////////
@@ -58,7 +58,7 @@ export default async function postCreate() {
   // SET PROJECT ENVIRONMENT VARIABLES
   //////////////////////////////////////////////////////////////////////////////////
 
-  if (EXPORT_PROJECT_ENV === "true") {
+  if (EXPORT_PROJECT_ENV === 'true') {
     await $`${run} vault kv export`;
   }
 
@@ -70,7 +70,7 @@ export default async function postCreate() {
   // SET NPM CREDENTIALS
   //////////////////////////////////////////////////////////////////////////////////
 
-  if (SET_NPM_CREDENTIALS === "true") {
+  if (SET_NPM_CREDENTIALS === 'true') {
     const NPMRC_INSTALL = process.env.NPMRC_INSTALL;
     const NPMRC_PUBLISH = process.env.NPMRC_PUBLISH;
     await $`echo ${NPMRC_INSTALL} | base64 -di -w 0 >${HOME}/.npmrc`;
@@ -85,7 +85,7 @@ export default async function postCreate() {
     const GCP_SERVICE_ACCOUNT_ADMIN = process.env.GCP_SERVICE_ACCOUNT_ADMIN;
     const GCP_SERVICE_ACCOUNT_RUN = process.env.GCP_SERVICE_ACCOUNT_RUN;
 
-    $.shell = "/usr/bin/zsh";
+    $.shell = '/usr/bin/zsh';
 
     try {
       $.verbose = false;
@@ -100,14 +100,14 @@ export default async function postCreate() {
       const isProjectExists =
         await $`gcloud projects list --filter="${GCP_PROJECT_NAME}"`;
 
-      if (`${isProjectExists}` != "") {
+      if (`${isProjectExists}` != '') {
         await $`gcloud config set project ${GCP_PROJECT_NAME}`;
         await $`gcloud config set compute/zone us-central1-b`;
         await $`gcloud auth configure-docker gcr.io --quiet`;
       }
     } catch (e) {
       console.log(chalk.red(e));
-      console.log("something went wrong");
+      console.log('something went wrong');
     }
 
     await sleep(2000);
@@ -117,7 +117,7 @@ export default async function postCreate() {
   // // GAM
   // //////////////////////////////////////////////////////////////////////////////////
 
-  if (SET_GAM_CREDENTIALS === "true") {
+  if (SET_GAM_CREDENTIALS === 'true') {
     const GAM_OAUTH2CLIENT = process.env.GAM_OAUTH2CLIENT;
     const GAM_CLIENTSECRETS = process.env.GAM_CLIENTSECRETS;
     const GAM_OAUTH2TXT = process.env.GAM_OAUTH2TXT;
@@ -133,7 +133,7 @@ export default async function postCreate() {
 
   const dotfilesFolder = fs.readdirSync(`${process.env.HOME}`);
 
-  if (!dotfilesFolder.includes(".dotfiles")) {
+  if (!dotfilesFolder.includes('.dotfiles')) {
     await $`git clone https://github.com/ghostmind-dev/dotfiles.git ${HOME}/.dotfiles`;
     await $`rcup -d ${HOME}/.dotfiles -x Readme.md -x .gitignore -f`;
   }
@@ -142,7 +142,7 @@ export default async function postCreate() {
   // // INSTALL APP DEPENDENCIES
   // ////////////////////////////////////////////////////////////////////////////////
 
-  if (INSTALL_DEV_DEPENDENCIES === "true") {
+  if (INSTALL_DEV_DEPENDENCIES === 'true') {
     await $`${run} utils dev install`;
   }
 
@@ -150,9 +150,34 @@ export default async function postCreate() {
   // // INSTALL LIVE RUN
   // //////////////////////////////////////////////////////////////////////////////////
 
-  if (RESET_LIVE_RUN === "true") {
+  if (RESET_LIVE_RUN === 'true') {
     await $`rm -rf ${SRC}/dev`;
     await $`git clone https://github.com/ghostmind-dev/run.git ${SRC}/dev`;
     await $`npm --prefix ${SRC}/dev  install`;
   }
+
+  //////////////////////////////////////////////////////////////////////////////////
+  //  SET DEVCONTAINER DEV ENVIRONMENT
+  //////////////////////////////////////////////////////////////////////////////////
+
+  $.verbose = false;
+
+  const currentBranchRaw = await $`git branch --show-current`;
+  // trim the trailing newline
+  const currentBranch = currentBranchRaw.stdout.trim();
+
+  let environemnt;
+  if (currentBranch === 'main') {
+    environemnt = 'prod';
+  } else if (currentBranch === 'preview') {
+    environemnt = 'preview';
+  } else {
+    environemnt = 'dev';
+  }
+
+  $.verbose = true;
+
+  // set environment name in zshenv
+
+  await $`echo "export ENVIRONMENT=${environemnt}" > ${HOME}/.zshenv`;
 }
