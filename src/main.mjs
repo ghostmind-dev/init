b654d2e41d27eb91f4c91387dc830f4e81de960d
Francois Seguin <seguin.mtl@gmail.com>
2024-01-27 20:21:35 +0000

    cleaning

--- src/main.mjs
+++ src/main.mjs
@@ -1,21 +1,22 @@
-const NODE_PATH = "/home/vscode/.npm-global/lib/node_modules";
+// const NODE_PATH = "/home/vscode/.npm-global/lib/node_modules";
 
-// import { $, fs, chalk, sleep } from 'zx';
+import { $, fs, chalk, sleep } from 'zx';
+import { config } from 'dotenv';
 
-const {
-  default: { config },
-} = await import(
-  `${NODE_PATH}/@ghostmind-dev/post-create/node_modules/dotenv/lib/main.js`
-);
+// const {
+//   default: { config },
+// } = await import(
+//   `${NODE_PATH}/@ghostmind-dev/post-create/node_modules/dotenv/lib/main.js`
+// );
 
-// const { config } = dotenv;
+// // const { config } = dotenv;
 
-const { $, fs, chalk, sleep } = await import(
-  `${NODE_PATH}/@ghostmind-dev/post-create/node_modules/zx/build/index.js`
-);
+// const { $, fs, chalk, sleep } = await import(
+//   `${NODE_PATH}/@ghostmind-dev/post-create/node_modules/zx/build/index.js`
+// );
 
 export default async function postCreate() {
-  console.log(chalk.blue("Starting devcontainer..."));
+  console.log(chalk.blue('Starting devcontainer...'));
   //////////////////////////////////////////////////////////////////////////////////
   // CONSTANTS
   //////////////////////////////////////////////////////////////////////////////////
@@ -31,14 +32,14 @@ export default async function postCreate() {
   //
   const {
     INIT_SRC = process.env.INIT_SRC,
-    INIT_EXPORT_ENV_PROJECT = "false",
-    INIT_EXPORT_ENV_ALL = "false",
-    INIT_DEV_INSTALL_DEPENDENCIES = "false",
-    INIT_DEV_RESET_LIVE = "false",
-    INIT_LOGIN_NPM = "false",
-    INIT_LOGIN_GCP = "false",
-    INIT_LOGIN_GAM = "false",
-    INIT_LOGIN_VAULT = "false",
+    INIT_EXPORT_ENV_PROJECT = 'false',
+    INIT_EXPORT_ENV_ALL = 'false',
+    INIT_DEV_INSTALL_DEPENDENCIES = 'false',
+    INIT_DEV_RESET_LIVE = 'false',
+    INIT_LOGIN_NPM = 'false',
+    INIT_LOGIN_GCP = 'false',
+    INIT_LOGIN_GAM = 'false',
+    INIT_LOGIN_VAULT = 'false',
   } = process.env;
   //////////////////////////////////////////////////////////////////////////////////
   // NPM GITHUB REGISTRY
@@ -60,24 +61,24 @@ export default async function postCreate() {
   //////////////////////////////////////////////////////////////////////////////////
   // VAULT LOGIN
   //////////////////////////////////////////////////////////////////////////////////
-  if (INIT_LOGIN_VAULT === "true") {
+  if (INIT_LOGIN_VAULT === 'true') {
     await $`vault login ${process.env.VAULT_ROOT_TOKEN}`;
-    if (INIT_EXPORT_ENV_PROJECT === "true") {
+    if (INIT_EXPORT_ENV_PROJECT === 'true') {
       await $`${run} vault kv export`;
     }
-    if (INIT_EXPORT_ENV_ALL === "true") {
+    if (INIT_EXPORT_ENV_ALL === 'true') {
       await $`${run} vault kv export --all`;
     }
   }
   //////////////////////////////////////////////////////////////////////////////////
   // SET PROJECT ENVIRONMENT VARIABLES
   //////////////////////////////////////////////////////////////////////////////////
-  const { config } = await import(`${SRC}/node_modules/dotenv/lib/main.js`);
+  // const { config } = await import(`${SRC}/node_modules/dotenv/lib/main.js`);
   config({ path: `${SRC}/.env` });
   //////////////////////////////////////////////////////////////////////////////////
   // SET NPM CREDENTIALS
   //////////////////////////////////////////////////////////////////////////////////
-  if (INIT_LOGIN_NPM === "true") {
+  if (INIT_LOGIN_NPM === 'true') {
     const NPMRC_INSTALL = process.env.NPMRC_INSTALL;
     const NPMRC_PUBLISH = process.env.NPMRC_PUBLISH;
     await $`echo ${NPMRC_INSTALL} | base64 -di -w 0 >${HOME}/.npmrc`;
@@ -86,9 +87,9 @@ export default async function postCreate() {
   // /////////////////////////////////////////////////////////////////////////////////
   // // GCP
   // ////////////////////////////////////////////////////////////////////////////////
-  if (INIT_LOGIN_GCP === "true") {
+  if (INIT_LOGIN_GCP === 'true') {
     const GCP_SERVICE_ACCOUNT_ADMIN = process.env.GCP_SERVICE_ACCOUNT_ADMIN;
-    $.shell = "/usr/bin/zsh";
+    $.shell = '/usr/bin/zsh';
     const GCP_PROJECT_NAME = process.env.GCP_PROJECT_NAME;
     try {
       $.verbose = false;
@@ -97,21 +98,21 @@ export default async function postCreate() {
       await $`gcloud auth activate-service-account --key-file="/tmp/gsa_key.json"`;
       const isProjectExists =
         await $`gcloud projects list --filter="${GCP_PROJECT_NAME}"`;
-      if (`${isProjectExists}` != "") {
+      if (`${isProjectExists}` != '') {
         await $`gcloud config set project ${GCP_PROJECT_NAME}`;
         await $`gcloud config set compute/zone us-central1-b`;
         await $`gcloud auth configure-docker gcr.io --quiet`;
       }
     } catch (e) {
       console.log(chalk.red(e));
-      console.log("something went wrong");
+      console.log('something went wrong');
     }
     await sleep(2000);
   }
   // //////////////////////////////////////////////////////////////////////////////////
   // // GAM
   // //////////////////////////////////////////////////////////////////////////////////
-  if (INIT_LOGIN_GAM === "true") {
+  if (INIT_LOGIN_GAM === 'true') {
     const GAM_OAUTH2CLIENT = process.env.GAM_OAUTH2CLIENT;
     const GAM_CLIENTSECRETS = process.env.GAM_CLIENTSECRETS;
     const GAM_OAUTH2TXT = process.env.GAM_OAUTH2TXT;
@@ -128,20 +129,20 @@ export default async function postCreate() {
   // DOTFILES
   //////////////////////////////////////////////////////////////////////////////////
   const dotfilesFolder = fs.readdirSync(`${process.env.HOME}`);
-  if (!dotfilesFolder.includes(".dotfiles")) {
+  if (!dotfilesFolder.includes('.dotfiles')) {
     await $`git clone https://github.com/ghostmind-dev/dotfiles.git ${HOME}/.dotfiles`;
     await $`rcup -d ${HOME}/.dotfiles -x Readme.md -x .gitignore -f`;
   }
   // ////////////////////////////////////////////////////////////////////////////////
   // // INSTALL APP DEPENDENCIES
   // ////////////////////////////////////////////////////////////////////////////////
-  if (INIT_DEV_INSTALL_DEPENDENCIES === "true") {
+  if (INIT_DEV_INSTALL_DEPENDENCIES === 'true') {
     await $`${run} utils dev install`;
   }
   // //////////////////////////////////////////////////////////////////////////////////
   // // INSTALL LIVE RUN
   // //////////////////////////////////////////////////////////////////////////////////
-  if (INIT_DEV_RESET_LIVE === "true") {
+  if (INIT_DEV_RESET_LIVE === 'true') {
     await $`rm -rf ${SRC}/dev`;
     await $`git clone https://github.com/ghostmind-dev/run.git ${SRC}/dev`;
     await $`npm --prefix ${SRC}/dev  install`;
@@ -154,12 +155,12 @@ export default async function postCreate() {
   // trim the trailing newline
   const currentBranch = currentBranchRaw.stdout.trim();
   let environemnt;
-  if (currentBranch === "main") {
-    environemnt = "prod";
-  } else if (currentBranch === "preview") {
-    environemnt = "preview";
+  if (currentBranch === 'main') {
+    environemnt = 'prod';
+  } else if (currentBranch === 'preview') {
+    environemnt = 'preview';
   } else {
-    environemnt = "dev";
+    environemnt = 'dev';
   }
   $.verbose = true;
   // set environment name in zshenv
