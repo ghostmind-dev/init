893588e3fdba2bbd5f48b77bd16a3a815c6b741f
Francois Seguin <seguin.mtl@gmail.com>
2024-02-19 00:17:22 +0000

    updating

--- app/bin/cmd.mjs
+++ app/bin/cmd.mjs
@@ -4,6 +4,10 @@ import { postCreateCommand } from '../main.mjs';
 
 const functionToRun = process.argv[2];
 
-if (functionToRun === 'postCreateCommand') {
+if (
+  functionToRun === 'postCreateCommand' ||
+  functionToRun === 'poat-create' ||
+  functionToRun === 'postcreate'
+) {
   await postCreateCommand();
 }
