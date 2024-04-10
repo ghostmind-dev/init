3e73945dc42b36241f9f32ffb7a13573b296be3d
Francois Seguin <seguin.mtl@gmail.com>
2024-04-10 00:17:38 +0000

    cleaning

--- init/main.ts
+++ init/main.ts
@@ -1,219 +1,218 @@
-// const NODE_PATH = "/home/vscode/.npm-global/lib/node_modules";
+#!/usr/bin/env -S deno run --allow-all
 
 import { $, fs, chalk, sleep, cd } from 'npm:zx';
 import { config } from 'npm:dotenv';
 
-export async function postCreateCommand() {
-  $.verbose = true;
+$.verbose = true;
 
-  console.log(chalk.blue('Starting devcontainer...'));
-  //////////////////////////////////////////////////////////////////////////////////
-  // CONSTANTS
-  //////////////////////////////////////////////////////////////////////////////////
-  const HOME = Deno.env.get('HOME');
-  const SRC = Deno.env.get('SRC');
-  // need a funny name for naming my environment variables
-  // INIT = devcontainer post create
-  // but this is not funny or original
-  // so I will use INIT = devcontainer post create
-  // propose a better name if you have one
-  // I will use it and give you credit
-  // INIT = devcontainer post create
-  //
-  const {
-    INIT_EXPORT_ENV_PROJECT = 'false',
-    INIT_EXPORT_ENV_ALL = 'false',
-    INIT_DEV_INSTALL_DEPENDENCIES = 'false',
-    INIT_DEV_RESET_LIVE = 'false',
-    INIT_LOGIN_NPM = 'false',
-    INIT_LOGIN_GCP = 'false',
-    INIT_LOGIN_GAM = 'false',
-    INIT_LOGIN_VAULT = 'false',
-    INIT_LOGIN_CLOUDFLARED = 'false',
-    INIT_SSH_MODE = 'false',
-  } = Deno.env.toObject();
-  //////////////////////////////////////////////////////////////////////////////////
-  // NPM GITHUB REGISTRY
-  //////////////////////////////////////////////////////////////////////////////////
-  await $`mkdir -p ${HOME}/.npm-global`;
-  await $`npm config set prefix ${HOME}/.npm-global`;
-  await $`npm config set update-notifier false`;
-  await $`export PATH=${HOME}/.npm-global/bin:$PATH`;
+console.log(chalk.blue('Starting devcontainer...'));
+//////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+//////////////////////////////////////////////////////////////////////////////////
+const HOME = Deno.env.get('HOME');
+const SRC = Deno.env.get('SRC');
 
-  //////////////////////////////////////////////////////////////////////////////////
-  // NPM PROJECT MODULES
-  //////////////////////////////////////////////////////////////////////////////////
+//
+const {
+  INIT_EXPORT_ENV_PROJECT = 'false',
+  INIT_EXPORT_ENV_ALL = 'false',
+  INIT_DEV_INSTALL_DEPENDENCIES = 'false',
+  INIT_DEV_RESET_LIVE = 'false',
+  INIT_LOGIN_NPM = 'false',
+  INIT_LOGIN_GCP = 'false',
+  INIT_LOGIN_GAM = 'false',
+  INIT_LOGIN_VAULT = 'false',
+  INIT_LOGIN_CLOUDFLARED = 'false',
+  INIT_SSH_MODE = 'false',
+} = Deno.env.toObject();
+//////////////////////////////////////////////////////////////////////////////////
+// NPM GITHUB REGISTRY
+//////////////////////////////////////////////////////////////////////////////////
+await $`mkdir -p ${HOME}/.npm-global`;
+await $`npm config set prefix ${HOME}/.npm-global`;
+await $`npm config set update-notifier false`;
+await $`export PATH=${HOME}/.npm-global/bin:$PATH`;
 
-  const fileExists = await fs.exists(`${SRC}/package.json`);
+//////////////////////////////////////////////////////////////////////////////////
+// NPM PROJECT MODULES
+//////////////////////////////////////////////////////////////////////////////////
 
-  if (fileExists) {
-    await $`npm install ${SRC}`;
-    await $`npm update`;
-  }
+const fileExists = await fs.exists(`${SRC}/package.json`);
 
-  //////////////////////////////////////////////////////////////////////////////////
-  // INSTALL RUN (PRODUCTION)
-  //////////////////////////////////////////////////////////////////////////////////
-
-  await $`git clone https://github.com/ghostmind-dev/run.git ${HOME}/run`;
-  await $`deno install --allow-all --force --name run ${HOME}/run/run/bin/cmd.ts`;
-  const run = `${HOME}/.deno/bin/run`;
-
-  //////////////////////////////////////////////////////////////////////////////////
-  // NPM GLOBAL MODULES (TO BE MOVED TO DVC)
-  //////////////////////////////////////////////////////////////////////////////////
-
-  await $`npm install -g npm-run-all`;
-
-  //////////////////////////////////////////////////////////////////////////////////
-  // SET DENO.JSON
-  //////////////////////////////////////////////////////////////////////////////////
-
-  await $`curl -o ${HOME}/deno.json https://raw.githubusercontent.com/ghostmind-dev/run/main/deno.json`;
-
-  //////////////////////////////////////////////////////////////////////////////////
-  // NPM GLOBAL MODULES (TO BE MOVED TO DVC)
-  //////////////////////////////////////////////////////////////////////////////////
-
-  if (INIT_SSH_MODE === 'true') {
-    config({ path: `${SRC}/.env` });
-  }
-
-  //////////////////////////////////////////////////////////////////////////////////
-  // VAULT LOGIN
-  //////////////////////////////////////////////////////////////////////////////////
-  if (INIT_LOGIN_VAULT === 'true') {
-    await $`vault login ${Deno.env.get('VAULT_ROOT_TOKEN')}`;
-    if (INIT_EXPORT_ENV_PROJECT === 'true') {
-      await $`${run} vault kv export`;
-    }
-    if (INIT_EXPORT_ENV_ALL === 'true') {
-      await $`${run} vault kv export --all`;
-    }
-  }
-  //////////////////////////////////////////////////////////////////////////////////
-  // SET PROJECT ENVIRONMENT VARIABLES
-  //////////////////////////////////////////////////////////////////////////////////
-
-  config({ path: `${SRC}/.env` });
-
-  //////////////////////////////////////////////////////////////////////////////////
-  // SET NPM CREDENTIALS
-  //////////////////////////////////////////////////////////////////////////////////
-
-  if (INIT_LOGIN_NPM === 'true') {
-    const NPM_TOKEN = Deno.env.get('NPM_TOKEN');
-
-    await $`echo //registry.npmjs.org/:_authToken=${NPM_TOKEN} >${SRC}/.npmrc`;
-    await $`echo //registry.npmjs.org/:_authToken=${NPM_TOKEN} >${HOME}/.npmrc`;
-  }
-  /////////////////////////////////////////////////////////////////////////////////
-  // GCP
-  ////////////////////////////////////////////////////////////////////////////////
-  if (INIT_LOGIN_GCP === 'true') {
-    const GCP_SERVICE_ACCOUNT_ADMIN = Deno.env.get('GCP_SERVICE_ACCOUNT_ADMIN');
-    $.shell = '/usr/bin/zsh';
-    const GCP_PROJECT_NAME = Deno.env.get('GCP_PROJECT_NAME');
-    try {
-      $.verbose = false;
-      await $`echo ${GCP_SERVICE_ACCOUNT_ADMIN} | base64 -di -w 0 >/tmp/gsa_key.json`;
-      $.verbose = true;
-      await $`gcloud auth activate-service-account --key-file="/tmp/gsa_key.json"`;
-      const isProjectExists =
-        await $`gcloud projects list --filter="${GCP_PROJECT_NAME}"`;
-      if (`${isProjectExists}` != '') {
-        await $`gcloud config set project ${GCP_PROJECT_NAME}`;
-        await $`gcloud config set compute/zone us-central1-b`;
-        await $`gcloud auth configure-docker gcr.io --quiet`;
-      }
-    } catch (e) {
-      console.log(chalk.red(e));
-      console.log('something went wrong');
-    }
-    await sleep(2000);
-  }
-  // //////////////////////////////////////////////////////////////////////////////////
-  // // GAM
-  // //////////////////////////////////////////////////////////////////////////////////
-  if (INIT_LOGIN_GAM === 'true') {
-    const GAM_OAUTH2CLIENT = Deno.env.get('GAM_OAUTH2CLIENT');
-    const GAM_CLIENTSECRETS = Deno.env.get('GAM_CLIENTSECRETS');
-    const GAM_OAUTH2TXT = Deno.env.get('GAM_OAUTH2TXT');
-    await $`echo ${GAM_OAUTH2CLIENT} | base64 -di -w 0 >/home/vscode/bin/gam/oauth2service.json`;
-    await $`echo ${GAM_CLIENTSECRETS} | base64 -di -w 0 >/home/vscode/bin/gam/client_secrets.json`;
-    await $`echo ${GAM_OAUTH2TXT} | base64 -di -w 0 >/home/vscode/bin/gam/oauth2.txt`;
-  }
-  //////////////////////////////////////////////////////////////////////////////////
-  // GIT SAFE
-  //////////////////////////////////////////////////////////////////////////////////
-  Deno.chdir(SRC);
-  await $`git config --add safe.directory "*"`;
-  //
-  //////////////////////////////////////////////////////////////////////////////////
-  // DOTFILES
-  //////////////////////////////////////////////////////////////////////////////////
-
-  await $`curl -o ${HOME}/.zshrc https://raw.githubusercontent.com/ghostmind-dev/dotfiles/main/config/zsh/.zshrc`;
-
-  ////////////////////////////////////////////////////////////////////////////////
-  // INSTALL APP DEPENDENCIES
-  ////////////////////////////////////////////////////////////////////////////////
-  if (INIT_DEV_INSTALL_DEPENDENCIES === 'true') {
-    await $`${run} utils dev install`;
-  }
-  //////////////////////////////////////////////////////////////////////////////////
-  // INSTALL LIVE RUN
-  //////////////////////////////////////////////////////////////////////////////////
-  if (INIT_DEV_RESET_LIVE === 'true') {
-    await $`rm -rf ${SRC}/dev`;
-    await $`git clone https://github.com/ghostmind-dev/run.git ${SRC}/dev`;
-    await $`deno install --allow-all --force --name live ${SRC}/dev/run/bin/cmd.ts`;
-  }
-
-  //////////////////////////////////////////////////////////////////////////////////
-  // SET CLOUDFLARED
-  //////////////////////////////////////////////////////////////////////////////////
-
-  if (INIT_LOGIN_CLOUDFLARED === 'true') {
-    const CLOUDFLARED_CREDS = Deno.env.get('CLOUDFLARED_CREDS');
-    $.shell = '/usr/bin/zsh';
-
-    try {
-      $.verbose = false;
-      await $`mkdir -p /home/vscode/.cloudflared`;
-      await $`echo ${CLOUDFLARED_CREDS} | base64 -di -w 0 > /home/vscode/.cloudflared/cert.pem`;
-    } catch (e) {
-      console.log(chalk.red(e));
-      console.log('something went wrong');
-    }
-    await sleep(2000);
-  }
-
-  //////////////////////////////////////////////////////////////////////////////////
-  //  SET DEVCONTAINER DEV ENVIRONMENT
-  //////////////////////////////////////////////////////////////////////////////////
-  $.verbose = false;
-  const currentBranchRaw = await $`git branch --show-current`;
-  // trim the trailing newline
-  const currentBranch = currentBranchRaw.stdout.trim();
-  let environemnt;
-  if (currentBranch === 'main') {
-    environemnt = 'prod';
-  } else if (currentBranch === 'preview') {
-    environemnt = 'preview';
-  } else {
-    environemnt = 'dev';
-  }
-  $.verbose = true;
-  // set environment name in zshenv
-  await $`echo "export ENV=${environemnt}" > ${HOME}/.zshenv`;
-  ////////////////////////////////////////////////////////////////////////////////
-  // CONNECT TO GHCR.IO
-  ////////////////////////////////////////////////////////////////////////////////
-  await $`echo ${Deno.env.get(
-    'GH_TOKEN'
-  )} | docker login ghcr.io -u USERNAME --password-stdin`;
-  ////////////////////////////////////////////////////////////////////////////////
-  // THE END
-  ////////////////////////////////////////////////////////////////////////////////
+if (fileExists) {
+  await $`npm install ${SRC}`;
+  await $`npm update`;
 }
+
+//////////////////////////////////////////////////////////////////////////////////
+// INSTALL RUN (PRODUCTION)
+//////////////////////////////////////////////////////////////////////////////////
+
+await $`git clone https://github.com/ghostmind-dev/run.git ${HOME}/run`;
+await $`deno install --allow-all --force --name run ${HOME}/run/run/bin/cmd.ts`;
+const run = `${HOME}/.deno/bin/run`;
+
+//////////////////////////////////////////////////////////////////////////////////
+// NPM GLOBAL MODULES (TO BE MOVED TO DVC)
+//////////////////////////////////////////////////////////////////////////////////
+
+await $`npm install -g npm-run-all`;
+
+//////////////////////////////////////////////////////////////////////////////////
+// SET DENO.JSON
+//////////////////////////////////////////////////////////////////////////////////
+
+await $`curl -o ${HOME}/deno.json https://raw.githubusercontent.com/ghostmind-dev/run/main/deno.json`;
+
+//////////////////////////////////////////////////////////////////////////////////
+// NPM GLOBAL MODULES (TO BE MOVED TO DVC)
+//////////////////////////////////////////////////////////////////////////////////
+
+if (INIT_SSH_MODE === 'true') {
+  config({ path: `${SRC}/.env` });
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+// VAULT LOGIN
+//////////////////////////////////////////////////////////////////////////////////
+if (INIT_LOGIN_VAULT === 'true') {
+  await $`vault login ${Deno.env.get('VAULT_ROOT_TOKEN')}`;
+  if (INIT_EXPORT_ENV_PROJECT === 'true') {
+    await $`${run} vault kv export`;
+  }
+  if (INIT_EXPORT_ENV_ALL === 'true') {
+    await $`${run} vault kv export --all`;
+  }
+}
+//////////////////////////////////////////////////////////////////////////////////
+// SET PROJECT ENVIRONMENT VARIABLES
+//////////////////////////////////////////////////////////////////////////////////
+
+config({ path: `${SRC}/.env` });
+
+//////////////////////////////////////////////////////////////////////////////////
+// SET NPM CREDENTIALS
+//////////////////////////////////////////////////////////////////////////////////
+
+if (INIT_LOGIN_NPM === 'true') {
+  const NPM_TOKEN = Deno.env.get('NPM_TOKEN');
+
+  await $`echo //registry.npmjs.org/:_authToken=${NPM_TOKEN} >${SRC}/.npmrc`;
+  await $`echo //registry.npmjs.org/:_authToken=${NPM_TOKEN} >${HOME}/.npmrc`;
+}
+/////////////////////////////////////////////////////////////////////////////////
+// GCP
+////////////////////////////////////////////////////////////////////////////////
+
+if (INIT_LOGIN_GCP === 'true') {
+  const GCP_SERVICE_ACCOUNT_ADMIN = Deno.env.get('GCP_SERVICE_ACCOUNT_ADMIN');
+  $.shell = '/usr/bin/zsh';
+  const GCP_PROJECT_NAME = Deno.env.get('GCP_PROJECT_NAME');
+  try {
+    $.verbose = false;
+    await $`echo ${GCP_SERVICE_ACCOUNT_ADMIN} | base64 -di -w 0 >/tmp/gsa_key.json`;
+    $.verbose = true;
+    await $`gcloud auth activate-service-account --key-file="/tmp/gsa_key.json"`;
+    const isProjectExists =
+      await $`gcloud projects list --filter="${GCP_PROJECT_NAME}"`;
+    if (`${isProjectExists}` != '') {
+      await $`gcloud config set project ${GCP_PROJECT_NAME}`;
+      await $`gcloud config set compute/zone us-central1-b`;
+      await $`gcloud auth configure-docker gcr.io --quiet`;
+    }
+  } catch (e) {
+    console.log(chalk.red(e));
+    console.log('something went wrong');
+  }
+  await sleep(2000);
+}
+// //////////////////////////////////////////////////////////////////////////////////
+// // GAM
+// //////////////////////////////////////////////////////////////////////////////////
+
+if (INIT_LOGIN_GAM === 'true') {
+  const GAM_OAUTH2CLIENT = Deno.env.get('GAM_OAUTH2CLIENT');
+  const GAM_CLIENTSECRETS = Deno.env.get('GAM_CLIENTSECRETS');
+  const GAM_OAUTH2TXT = Deno.env.get('GAM_OAUTH2TXT');
+  await $`echo ${GAM_OAUTH2CLIENT} | base64 -di -w 0 >/home/vscode/bin/gam/oauth2service.json`;
+  await $`echo ${GAM_CLIENTSECRETS} | base64 -di -w 0 >/home/vscode/bin/gam/client_secrets.json`;
+  await $`echo ${GAM_OAUTH2TXT} | base64 -di -w 0 >/home/vscode/bin/gam/oauth2.txt`;
+}
+//////////////////////////////////////////////////////////////////////////////////
+// GIT SAFE
+//////////////////////////////////////////////////////////////////////////////////
+Deno.chdir(SRC);
+await $`git config --add safe.directory "*"`;
+//
+//////////////////////////////////////////////////////////////////////////////////
+// DOTFILES
+//////////////////////////////////////////////////////////////////////////////////
+
+await $`curl -o ${HOME}/.zshrc https://raw.githubusercontent.com/ghostmind-dev/dotfiles/main/config/zsh/.zshrc`;
+
+////////////////////////////////////////////////////////////////////////////////
+// INSTALL APP DEPENDENCIES
+////////////////////////////////////////////////////////////////////////////////
+if (INIT_DEV_INSTALL_DEPENDENCIES === 'true') {
+  await $`${run} utils dev install`;
+}
+//////////////////////////////////////////////////////////////////////////////////
+// INSTALL LIVE RUN
+//////////////////////////////////////////////////////////////////////////////////
+if (INIT_DEV_RESET_LIVE === 'true') {
+  await $`rm -rf ${SRC}/dev`;
+  await $`git clone https://github.com/ghostmind-dev/run.git ${SRC}/dev`;
+  await $`deno install --allow-all --force --name live ${SRC}/dev/run/bin/cmd.ts`;
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+// SET CLOUDFLARED
+//////////////////////////////////////////////////////////////////////////////////
+
+if (INIT_LOGIN_CLOUDFLARED === 'true') {
+  const CLOUDFLARED_CREDS = Deno.env.get('CLOUDFLARED_CREDS');
+  $.shell = '/usr/bin/zsh';
+
+  try {
+    $.verbose = false;
+    await $`mkdir -p /home/vscode/.cloudflared`;
+    await $`echo ${CLOUDFLARED_CREDS} | base64 -di -w 0 > /home/vscode/.cloudflared/cert.pem`;
+  } catch (e) {
+    console.log(chalk.red(e));
+    console.log('something went wrong');
+  }
+  await sleep(2000);
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+//  SET DEVCONTAINER DEV ENVIRONMENT
+//////////////////////////////////////////////////////////////////////////////////
+
+$.verbose = false;
+
+const currentBranchRaw = await $`git branch --show-current`;
+// trim the trailing newline
+const currentBranch = currentBranchRaw.stdout.trim();
+let environemnt;
+if (currentBranch === 'main') {
+  environemnt = 'prod';
+} else if (currentBranch === 'preview') {
+  environemnt = 'preview';
+} else {
+  environemnt = 'dev';
+}
+$.verbose = true;
+// set environment name in zshenv
+await $`echo "export ENV=${environemnt}" > ${HOME}/.zshenv`;
+
+////////////////////////////////////////////////////////////////////////////////
+// CONNECT TO GHCR.IO
+////////////////////////////////////////////////////////////////////////////////
+
+await $`echo ${Deno.env.get(
+  'GH_TOKEN'
+)} | docker login ghcr.io -u USERNAME --password-stdin`;
+
+////////////////////////////////////////////////////////////////////////////////
+// THE END
+////////////////////////////////////////////////////////////////////////////////
