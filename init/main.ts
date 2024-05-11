09dbd68fa572912c0899c07af2bc843c3b85ff12
Francois Seguin <seguin.mtl@gmail.com>
2024-05-11 19:36:48 -0400

    updating

--- init/main.ts
+++ init/main.ts
@@ -192,6 +192,24 @@ if (INIT_RESET_LIVE === 'true') {
   denoConfig.compilerOptions.types = [`${SRC}/dev/run/types/global.d.ts`];
 
   await fs.writeJson(`${HOME}/deno.json`, denoConfig, { spaces: 2 });
+} else {
+  // verify if ${SRC}/dev exists
+
+  const devExists = await fs.exists(`${SRC}/dev`);
+
+  if (devExists) {
+    await $`deno install --allow-all --force --name live ${SRC}/dev/run/bin/cmd.ts`;
+
+    // get deno.json and replace a property and write it back
+
+    const denoConfig = await fs.readJson(`${HOME}/deno.json`);
+
+    // replace compilerOptions.types with a new array
+
+    denoConfig.compilerOptions.types = [`${SRC}/dev/run/types/global.d.ts`];
+
+    await fs.writeJson(`${HOME}/deno.json`, denoConfig, { spaces: 2 });
+  }
 }
 
 //////////////////////////////////////////////////////////////////////////////////
